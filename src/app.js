9e8fd21ea672f7a2ec7fd196e1d7d5741d964060
Tushika <tushikagarg11@gmail.com>
2024-05-17 11:39:54 +0530

    connect database

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+  })
+)
+
+app.use(express.json())
+app.use(express.urlencoded())
+
+
+
+export {app}
---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+import { DB_NAME } from '../constants'
+
+const connectDB = async () => {
+  try {
+    const connectionInst = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    )
+    console.log('MONGODB connected successfully')
+  } catch (error) {
+    console.log(`MONGODB connection error: ${error}`)
+    process.exit(1)
+  }
+}
+
+export default connectDB
