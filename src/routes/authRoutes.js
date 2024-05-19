0e22717abf1ded85c8192a34e80c5bbdd7dbeb8e
Tushika <tushikagarg11@gmail.com>
2024-05-20 00:43:56 +0530

    add voucher routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { loginController, registerContoller } from '../controllers/authContoller.js';
+import { voucherCreationMiddleware } from '../middlewares/voucherCreationMiddleware.js';
+import { isAdmin, requireSignIn } from '../middlewares/authMiddleware.js';
 const router=express.Router();
 
 
 router.post('/register',registerContoller);
 router.post('/login',loginController);
-// router.get('./eligibleVouchers',requiresSignIn,eligibleController)
-// router.post('./generateVouchers',requiresSignIn,isAdmin,generateController)
-// router.get('./vouchersController',requiresSignIn,vouchersController)
+router.post('./eligibleVouchers',requireSignIn,eligibilityController)
+router.post('./generateVouchers',requireSignIn,isAdmin,voucherCreationMiddleware)
 
 export default router
