3c15369984023a81390cdaabb280ea4bd0d93724
Tushika <tushikagarg11@gmail.com>
2024-05-20 00:42:05 +0530

    create vouchers

--- src/controllers/generateController.js
+++ src/controllers/generateController.js
@@ -10,11 +10,11 @@ export const createCashVoucher = async (req, res) => {
       expirationDate
     } = req.body
 
-    if (!code || !discountAmount || !minimumOrderValue || !expirationDate) {
+    if (!code || !discountAmount || !minimumCartAmount || !expirationDate) {
       return res.status(400).send({
         success: false,
         message:
-          'Code,discountValue,minimumOrderValue,expirationDate are required'
+          'Code,discountValue,minimumCartAmount,expirationDate are required'
       })
     }
     const newVoucher = new voucherModel({
@@ -53,11 +53,11 @@ export const createPercentageVoucher = async (req, res) => {
       minimumCartAmount,
       expirationDate
     } = req.body
-    if (!code || !discountPercentage || !minimumOrderValue || !expirationDate) {
+    if (!code || !discountPercentage || !minimumCartAmount || !expirationDate) {
       return res.status(400).send({
         success: false,
         message:
-          'Code,discountPercentage,minimumOrderValue,expirationDate are required'
+          'Code,discountPercentage,minimumCartAmount,expirationDate are required'
       })
     }
     const newVoucher = new voucherModel({
@@ -82,6 +82,145 @@ export const createPercentageVoucher = async (req, res) => {
     res.status(500).send({
       success: false,
       message: 'Error creating percentage voucher',
+      error: err
+    })
+  }
+}
+
+export const createWalletPointsVoucher = async (req, res) => {
+  try {
+    const { code, walletPoints, description, expirationDate } = req.body
+    if (!code || !walletPoints || !description || !expirationDate) {
+      return res.status(400).send({
+        success: false,
+        message: 'Code,walletPoints,description,expirationDate are required'
+      })
+    }
+    const newVoucher = new voucherModel({
+      code,
+      type: 'walletPoints',
+      walletPoints,
+      description,
+      expirationDate,
+      createdAt: new Date()
+    })
+
+    await newVoucher.save()
+
+    res.status(201).send({
+      success: true,
+      message: 'walletPoint voucher created successfully',
+      voucher: newVoucher
+    })
+  } catch (err) {
+    console.log('Error creating voucher', err)
+    res.status(500).send({
+      success: false,
+      message: 'Error creating walletPoint voucher',
+      error: err
+    })
+  }
+}
+
+export const createFirstOrderVoucher = async (req, res) => {
+  try {
+    const { code, walletPoints, description, expirationDate } = req.body
+    if (!code || !walletPoints || !description || !expirationDate) {
+      return res.status(400).send({
+        success: false,
+        message: 'Code,walletPoints,description,expirationDate are required'
+      })
+    }
+    const newVoucher = new voucherModel({
+      code,
+      type: 'firstOrder',
+      walletPoints,
+      description,
+      expirationDate,
+      createdAt: new Date()
+    })
+
+    await newVoucher.save()
+
+    res.status(201).send({
+      success: true,
+      message: 'firstOrder voucher created successfully',
+      voucher: newVoucher
+    })
+  } catch (err) {
+    console.log('Error creating voucher', err)
+    res.status(500).send({
+      success: false,
+      message: 'Error creating firstOrder voucher',
+      error: err
+    })
+  }
+}
+
+export const createBirthdayGiftVoucher = async (req, res) => {
+  try {
+    const { code, walletCash, description, expirationDate } = req.body
+    if (!code || !walletCash || !description || !expirationDate) {
+      return res.status(400).send({
+        success: false,
+        message: 'Code,walletCash,description,expirationDate are required'
+      })
+    }
+    const newVoucher = new voucherModel({
+      code,
+      type: 'birthdayGift',
+      walletCash,
+      description,
+      expirationDate,
+      createdAt: new Date()
+    })
+
+    await newVoucher.save()
+
+    res.status(201).send({
+      success: true,
+      message: 'birthdayGift voucher created successfully',
+      voucher: newVoucher
+    })
+  } catch (err) {
+    console.log('Error creating voucher', err)
+    res.status(500).send({
+      success: false,
+      message: 'Error creating birthdayGift voucher',
+      error: err
+    })
+  }
+}
+
+export const createFreeShippingVoucher = async (req, res) => {
+  try {
+    const { code, description, expirationDate} = req.body
+    if (!code || !description || !expirationDate) {
+      return res.status(400).send({
+        success: false,
+        message: 'Code,description,expirationDate are required'
+      })
+    }
+    const newVoucher = new voucherModel({
+      code,
+      type: 'freeShipping',
+      description,
+      expirationDate,
+      createdAt: new Date()
+    })
+
+    await newVoucher.save()
+
+    res.status(201).send({
+      success: true,
+      message: 'freeShipping voucher created successfully',
+      voucher: newVoucher
+    })
+  } catch (err) {
+    console.log('Error creating voucher', err)
+    res.status(500).send({
+      success: false,
+      message: 'Error creating freeShipping voucher',
       error: err
     })
   }
