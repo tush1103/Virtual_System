73a31e653083ea62ab51c34135041d8681f3f915
Tushika <tushikagarg11@gmail.com>
2024-05-22 18:33:56 +0530

    place order

---
+++ src/controllers/placeOrderController.js
@@ -0,0 +1,56 @@
+import orderModel from '../models/order.model.js'
+import vouchersModel from '../models/vouchers.model.js'
+
+export const placeOrderController = async (req, res) => {
+  try {
+    const { total, voucherId } = req.body
+    const voucher=await vouchersModel.findById(voucherId) 
+    let newtotal=0;
+
+    //check for existing voucher
+    const existingVoucher=await orderModel.findOne({appliedVoucher: voucherId,userId:req.user._id});
+    if(existingVoucher) {
+        return res.status(400).send({success:false,message:"Voucher already applied"})
+    }
+
+    //check if voucher is valid or not
+    if(voucher.type=="discountPerc"){
+        newtotal=total-(total*(voucher.discountPercentage/100))
+    }
+    else if(voucher.type=="discountCash"){
+        newtotal=total-voucher.discountAmount
+    }
+    else if(voucher.type=="firstOrder"){
+        const firstOrder=await orderModel.findOne({userId:req.user._id});
+        if(!firstOrder){
+            newtotal=total-voucher.discountAmount
+        }
+        else{
+            return res.status(400).send({success:false,message:"This is not your first order"})
+        }
+    }
+    else{
+        return res.status(400).send({success:false,message:"Invalid voucher"})
+    }
+    const order = new orderModel({
+      userId: req.user._id,
+      appliedVoucher: voucherId,
+      total:newtotal,
+      redeemedAt: Date.now()
+    })
+    await order.save()
+    return res
+      .status(200)
+      .send({
+        success: true,
+        message: 'Order placed successfully',
+        totalBeforeVoucher:total,
+        totalAfterVoucher:newtotal,
+        voucher
+      })
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ success: false, message: 'Error placing order', error: error })
+  }
+}
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,14 +1,23 @@
-import express from 'express';
-import { loginController, registerContoller } from '../controllers/authContoller.js';
-import { voucherCreationMiddleware } from '../middlewares/voucherCreationMiddleware.js';
-import { isAdmin, requireSignIn } from '../middlewares/authMiddleware.js';
-import { eligibilityController } from '../controllers/eligibilityController.js';
-const router=express.Router();
+import express from 'express'
+import {
+  loginController,
+  registerContoller
+} from '../controllers/authContoller.js'
+import { createVoucher } from '../controllers/generateController.js'
+import { isAdmin, requireSignIn } from '../middlewares/authMiddleware.js'
+import { eligibilityController } from '../controllers/eligibilityController.js'
+import { placeOrderController } from '../controllers/placeOrderController.js';
+const router = express.Router()
 
-
-router.post('/register',registerContoller);
-router.post('/login',loginController);
-router.post('/eligibleVouchers',requireSignIn,eligibilityController)
-router.post('/generateVouchers',requireSignIn,isAdmin,voucherCreationMiddleware)
+router.post('/register', registerContoller)
+router.post('/login', loginController)
+router.post('/eligibleVouchers', requireSignIn, eligibilityController)
+router.post(
+  '/generateVouchers',
+  requireSignIn,
+  isAdmin,
+  createVoucher
+)
+router.post('/placeOrder',requireSignIn,placeOrderController)
 
 export default router
