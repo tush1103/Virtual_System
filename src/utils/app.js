cc71bfe871a86e4e5cdca503c129f63f468e6077
Tushika <tushikagarg11@gmail.com>
2024-06-09 22:32:34 +0530

    update fie structure

--- src/utils/app.js
+++ src/utils/app.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import authRoutes from './routes/authRoutes.js'
+import authRoutes from '../routes/authRoutes.js'
 import cookieParser from 'cookie-parser'
 const app = express()
 app.use(
